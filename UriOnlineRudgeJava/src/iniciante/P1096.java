4a103052a0cb4636b0d95f6c4260bd6fb76bbe55
KitKety <kety.diniz18@gmail.com>
2020-05-15 21:26:33 -0300

    P1096

---
+++ UriOnlineRudgeJava/src/iniciante/P1096.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+public class P1096 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int x = 0; x < 3; x++) {
+				System.out.printf("I=" + i + " J=" + (7 - x) + "\n");
+			}
+			i++;
+		}
+	}
+
+}
