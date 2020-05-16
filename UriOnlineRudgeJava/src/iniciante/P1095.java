d86a6aeab33089658de720d1633a55bc29842457
KitKety <kety.diniz18@gmail.com>
2020-05-15 21:20:36 -0300

    P1095

---
+++ UriOnlineRudgeJava/src/iniciante/P1095.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+public class P1095 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int j = 60;
+		while (j >= 0) {
+			System.out.printf("I=" + i + " J=" + j + "\n");
+			i+=3;
+			j-=5;
+		}
+	}
+}
