6205ba849453dcd7afe01abbb81f78334e1d4686
KitKety <kety.diniz18@gmail.com>
2020-06-13 23:56:47 -0300

    P2581

---
+++ UriOnlineRudgeJava/src/iniciante/P2581.java
@@ -0,0 +1,16 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class P2581 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		sc.close();
+		for (int i = 0; i < cases; i++) {
+			System.out.println("I am Toorg!");
+		}
+	}
+
+}
