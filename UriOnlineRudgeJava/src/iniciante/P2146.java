8d14916e4f69294027e57a7a4a451cca60fd05cf
KitKety <kety.diniz18@gmail.com>
2020-06-13 23:54:32 -0300

    P2146

---
+++ UriOnlineRudgeJava/src/iniciante/P2146.java
@@ -0,0 +1,15 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class P2146 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			System.out.println(sc.nextInt() - 1);
+		}
+		sc.close();
+	}
+
+}
