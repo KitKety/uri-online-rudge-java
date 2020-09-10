10e9a416c74bbd3900cbfd5dfb95664b06349027
KitKety <kety.diniz18@gmail.com>
2020-09-10 20:25:15 -0300

    P2752

---
+++ UriOnlineRudgeJava/src/iniciante/P2752.java
@@ -0,0 +1,18 @@
+package iniciante;
+
+public class P2752 {
+
+	public static void main(String[] args) {
+		String var = "AMO FAZER EXERCICIO NO URI";
+		
+		System.out.printf("<%s>\n", var);
+		System.out.printf("<%30s>\n", var);
+		System.out.printf("<%.20s>\n", var);
+		System.out.printf("<%-20s>\n", var);
+		System.out.printf("<%-30s>\n", var);
+		System.out.printf("<%.30s>\n", var);
+		System.out.printf("<%30.20s>\n", var);
+		System.out.printf("<%-30.20s>\n", var);
+	}
+
+}
