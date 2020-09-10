55dbfd0ee0663807f10c5af8795c335b3c07bac3
KitKety <kety.diniz18@gmail.com>
2020-09-10 20:25:34 -0300

    P2753

---
+++ UriOnlineRudgeJava/src/iniciante/P2753.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+public class P2753 {
+
+	public static void main(String[] args) {
+		char c = 'a';
+		for (int i = 97; i < 123; i++) {
+			System.out.println(i + " e " + c++);
+		}
+	}
+
+}
