1e921089c5f5ea0a62e8394fb5f90b2469c58552
KitKety <kety.diniz18@gmail.com>
2020-09-10 20:25:47 -0300

    P2755

---
+++ UriOnlineRudgeJava/src/iniciante/P2755.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+public class P2755 {
+
+	public static void main(String[] args) {
+		System.out.printf("\"Ro\'b\'er\tto\\/\"\n");
+		System.out.println("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+		System.out.println("(^_-) (-_-) (-_^)");
+		System.out.println("(\"_\") ('.')");
+	}
+
+}
