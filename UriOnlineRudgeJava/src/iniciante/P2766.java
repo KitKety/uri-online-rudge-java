576a9f6417bbcb88f80dcec8adbb157f649dd4e8
KitKety <kety.diniz18@gmail.com>
2020-09-13 19:07:09 -0300

    P2766

---
+++ UriOnlineRudgeJava/src/iniciante/P2766.java
@@ -0,0 +1,21 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class P2766 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		sc.nextLine();
+		System.out.println(sc.nextLine().trim());
+		sc.nextLine();
+		sc.nextLine();
+		sc.nextLine();
+		System.out.println(sc.nextLine().trim());
+		sc.nextLine();
+		System.out.println(sc.nextLine().trim());
+		sc.close();
+	}
+
+}
